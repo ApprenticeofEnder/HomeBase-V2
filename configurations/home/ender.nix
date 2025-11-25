bd2cf2594b08dcd9d46afb5d4f1e5393a45dd6e3
Robert Babaev <github@robertbabaev.tech>
2025-11-25 17:36:13 -0500

    feat: add config for ender

---
+++ configurations/home/ender.nix
@@ -0,0 +1,18 @@
+{flake, ...}: let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in {
+  imports = [
+    self.homeModules.default
+  ];
+
+  # Defined by /modules/home/me.nix
+  # And used all around in /modules/home/*
+  me = {
+    username = "ender";
+    fullname = "Robert Babaev";
+    email = "github@robertbabaev.tech";
+  };
+
+  home.stateVersion = "25.05";
+}
