6e988e7c4df893e8298b344ad5c5f404997de59d
Robert Babaev <github@robertbabaev.tech>
2025-11-25 21:51:41 -0500

    docs: add todo regarding gpu detection
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/services/ollama.nix
+++ modules/home/services/ollama.nix
@@ -1,6 +1,6 @@
 {
   services.ollama = {
-    enable = true;
+    enable = true; # TODO: Investigate detecting gpus
     port = 11434; # default
     host = "0.0.0.0"; # default
     # acceleration = "rocm";
