7e7a381fa76417037c9351816b8c91234a5e00aa
Robert Babaev <github@robertbabaev.tech>
2025-11-25 21:50:55 -0500

    feat: add some additional services
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

---
+++ modules/home/services/ssh-agent.nix
@@ -0,0 +1,11 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  services.ssh-agent = lib.mkIf pkgs.stdenv.isLinux {
+    enable = true;
+    # forwardAgent = false;
+    # socket = "ssh-agent"; # default
+  };
+}
