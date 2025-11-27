bffc4fd1893a5a177a6d4e87abd836313b97f2d7
Robert Babaev <github@robertbabaev.tech>
2025-11-27 13:06:54 -0500

    fix: remove nixvim
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- flake.nix
+++ flake.nix
@@ -19,9 +19,9 @@
     # Software inputs
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.inputs.flake-parts.follows = "flake-parts";
+    # nixvim.url = "github:nix-community/nixvim";
+    # nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    # nixvim.inputs.flake-parts.follows = "flake-parts";
   };
 
   # Wired using https://nixos-unified.org/guide/autowiring
